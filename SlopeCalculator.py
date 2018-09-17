fe608892fccb6e1fb4a45f1071832f7dc423f3a2
Jason Gao <jason.gao@OO-What-Are-Those.local>
2018-09-17 15:25:16 -0400

    Slope Calculator Completed

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -1,1 +1,11 @@
 print("- Slope Calculator -")
+#Input
+y2 = int(input("What is Y2?"))
+y1 = int(input("What is Y1?"))
+x2 = int(input("What is X2?"))
+x1 = int(input("What is X1?"))
+print("Your first point is:", (x1, y1))
+print("Your second point is:", (x2, y2))
+slope = float(y2 - y1) / (x2 - x1)
+#Process
+print("Your Slope is: ", slope)
