3fb1d2cb78011487924750aa73a3e06dccb622b1
Jason Gao <jason.gao@OO-What-Are-Those.local>
2018-09-25 09:09:07 -0400

    GUI process

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -1,4 +1,8 @@
+import os
+
 print("- Slope Calculator -")
+
+os.system("say This is the slope calculator")
 #Input
 y2 = int(input("What is Y2?"))
 y1 = int(input("What is Y1?"))
@@ -8,4 +12,5 @@ print("Your first point is:", (x1, y1))
 print("Your second point is:", (x2, y2))
 slope = float(y2 - y1) / (x2 - x1)
 #Process
+os.system(str("say Your Slope is" + slope))
 print("Your Slope is: ", slope)
